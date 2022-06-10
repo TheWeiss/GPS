483d4095abfb3812d4dabe95a1c7a8ffd6ad6cdd
The Weiss ;) <amitdweiss@gmail.com>
2022-06-10 20:47:55 +0300

    updated experiment so it can run autoxgb

--- gps_lib/experiment.py
+++ gps_lib/experiment.py
@@ -5,43 +5,92 @@ import pandas as pd
 import re
 import glob
 import numpy as np
-from autoxgb.cli.predict import PredictAutoXGBCommand
 from parse_raw_utils import get_isolate_features
 import data_sets as ds
+from autoxgb import AutoXGB
+from autoxgb.cli.predict import PredictAutoXGBCommand
 
 
-def run_h2o(model_param, exp_name):
-    print(exp_name)
-    # Import a sample binary outcome train/test set into H2O
-    trainH2o = h2o.import_file('../experiments/{}/train.csv'.format(exp_name))
-    print(trainH2o.as_data_frame().columns[-1])
-    testH2o = h2o.import_file('../experiments/{}/test.csv'.format(exp_name))
-    rangeH2o = h2o.import_file('../experiments/{}/X_range.csv'.format(exp_name))
+def run_h2o(exp_name, model_param, ds_param_files_path, col_names):
+    pass
+    # print(exp_name)
+    # # Import a sample binary outcome train/test set into H2O
+    # trainH2o = h2o.import_file('../experiments/{}/train.csv'.format(exp_name))
+    # print(trainH2o.as_data_frame().columns[-1])
+    # testH2o = h2o.import_file('../experiments/{}/test.csv'.format(exp_name))
+    # rangeH2o = h2o.import_file('../experiments/{}/X_range.csv'.format(exp_name))
+    # model_name = '_'.join(['_'.join([k, str(v)]) for k, v in model_param.items()])
+    #
+    # # Identify predictors and response
+    # x = list(pd.read_csv('../experiments/{}/features.csv'.format(exp_name))['features'].values)
+    # y = pd.read_csv('../experiments/{}/label.csv'.format(exp_name)).loc[0, 'label']
+    #
+    # # Run AutoML for 20 base models
+    # aml = H2OAutoML(max_models=model_param['max_models'], seed=42, max_runtime_secs=model_param['train_time'])
+    # aml.train(x=x, y=y, training_frame=trainH2o)
+    #
+    # # View the AutoML Leaderboard
+    # lb = h2o.automl.get_leaderboard(aml, extra_columns="ALL")
+    # lb.head(rows=lb.nrows)  # Print all rows instead of default (10 rows)
+    #
+    # model = aml.leader
+    # model_path = h2o.save_model(model=model, path='../experiments/{}/{}/model'.format(exp_name, model_name), force=True)
+    # lb.as_data_frame().to_csv('../experiments/{}/{}/leader_board.csv'.format(exp_name, model_name))
+    # test_preds = model.predict(testH2o).as_data_frame()
+    # range_preds = model.predict(rangeH2o).as_data_frame()
+    # train_preds = model.predict(trainH2o).as_data_frame()
+    # test_preds.to_csv('../experiments/{}/{}/test_preds.csv'.format(exp_name, model_name))
+    # range_preds.to_csv('../experiments/{}/{}/range_preds.csv'.format(exp_name, model_name))
+    # train_preds.to_csv('../experiments/{}/{}/train_preds.csv'.format(exp_name, model_name))
+    # return aml
+
+def run_autoxgb(exp_name, model_param, ds_param_files_path, col_names):
     model_name = '_'.join(['_'.join([k, str(v)]) for k, v in model_param.items()])
+    # required parameters:
+    train_filename = '{}/train.csv'.format(ds_param_files_path)
+    output = '../experiments/{}/{}/model'.format(exp_name, model_name)
+    if os.path.exists('../experiments/{}/{}'.format(exp_name, model_name)):
+        os.system("rm -rf " + '../experiments/{}/{}'.format(exp_name, model_name))
 
-    # Identify predictors and response
-    x = list(pd.read_csv('../experiments/{}/features.csv'.format(exp_name))['features'].values)
-    y = pd.read_csv('../experiments/{}/label.csv'.format(exp_name)).loc[0, 'label']
+    # optional parameters
+    test_filename = '{}/test.csv'.format(ds_param_files_path)
+    task = 'regression'
+    idx = 'run_id'
+    targets = [col_names['label']]
+    features = col_names['features']
+    categorical_features = None
+    use_gpu = True
+    num_folds = 5
+    seed = 42
+    num_trials = model_param['max_models']
+    time_limit = model_param['train_time']
+    fast = True
 
-    # Run AutoML for 20 base models
-    aml = H2OAutoML(max_models=model_param['max_models'], seed=42, max_runtime_secs=model_param['train_time'])
-    aml.train(x=x, y=y, training_frame=trainH2o)
-
-    # View the AutoML Leaderboard
-    lb = h2o.automl.get_leaderboard(aml, extra_columns="ALL")
-    lb.head(rows=lb.nrows)  # Print all rows instead of default (10 rows)
-
-    model = aml.leader
-    model_path = h2o.save_model(model=model, path='../experiments/{}/{}/model'.format(exp_name, model_name), force=True)
-    lb.as_data_frame().to_csv('../experiments/{}/{}/leader_board.csv'.format(exp_name, model_name))
-    test_preds = model.predict(testH2o).as_data_frame()
-    range_preds = model.predict(rangeH2o).as_data_frame()
-    train_preds = model.predict(trainH2o).as_data_frame()
-    test_preds.to_csv('../experiments/{}/{}/test_preds.csv'.format(exp_name, model_name))
-    range_preds.to_csv('../experiments/{}/{}/range_preds.csv'.format(exp_name, model_name))
-    train_preds.to_csv('../experiments/{}/{}/train_preds.csv'.format(exp_name, model_name))
-    return aml
-
+    # Now its time to train the model!
+    axgb = AutoXGB(
+        train_filename=train_filename,
+        output=output,
+        test_filename=test_filename,
+        task=task,
+        idx=idx,
+        targets=targets,
+        features=features,
+        categorical_features=categorical_features,
+        use_gpu=use_gpu,
+        num_folds=num_folds,
+        seed=seed,
+        num_trials=num_trials,
+        time_limit=time_limit,
+        fast=fast,
+    )
+    axgb.train()
+    PredictAutoXGBCommand('../experiments/{}/{}/model'.format(exp_name, model_name),
+                          '{}/range_X.csv'.format(ds_param_files_path),
+                          '../experiments/{}/{}/range_preds.csv'.format(exp_name, model_name)).execute()
+    os.rename("../experiments/{}/{}/model/oof_predictions.csv".format(exp_name, model_name),
+              "../experiments/{}/{}/train_preds.csv".format(exp_name, model_name))
+    os.rename("../experiments/{}/{}/model/test_predictions.csv".format(exp_name, model_name),
+              "../experiments/{}/{}/test_preds.csv".format(exp_name, model_name))
 
 def fill_all_results():
     h2o.init()
@@ -81,28 +130,31 @@ def walk_level(some_dir, level=1):
         if num_sep + level <= num_sep_this:
             del dirs[:]
 
-def run_exp(dataset: ds.MICDataSet, ds_param, antibiotic=None, species=None):
-    if ds_param is None:
-        pass
-        # default_values = {
-        #     'species_sep': True,
-        #     'antibiotic_sep': True,
-        #     'handle_range': 'remove',  # remove/strip/move
-        #     'handle_multi_mic': 'remove',  # remove/max/min/rand
-        #     'ignore_small_dilu': True,  # remove/max/min/rand
-        #     'task': 'regression',  # regression/classification/SIR
-        #     'log2': True,
-        #     'move_range_by': 5,
-        #     'reg_stratified': True,
-        #     'stratified_cv_num': 3,
-        # }
-    train, test, range_data, col_names, ds_param_files_path, antibiotic_name, species_name = dataset.generate_dataset(ds_param, antibiotic, species)
-    exp_name = '_'.join(ds_param_files_path.split('/')[-3::-1])
+def run_exp(dataset: ds.MICDataSet, model_param, ds_param=None, antibiotic=None, species=None):
+    if type(antibiotic)==list:
+        for antibiotic_i in antibiotic:
+            if type(species)==list:
+                for species_j in species:
+                    run_exp(dataset, model_param, ds_param, antibiotic_i, species_j)
+            else:
+                run_exp(dataset, model_param, ds_param, antibiotic_i, species)
+    else:
+        if type(species) == list:
+            for species_j in species:
+                run_exp(dataset, model_param, ds_param, antibiotic, species_j)
+        else:
+            train, test, range_X, range_y, col_names, ds_param_files_path, antibiotic_name, species_name, cv = data.generate_dataset(
+                ds_param, antibiotic, species)
+            exp_name = '_'.join([ds_param_files_path.split('/')[-3::][i] for i in [1, 2, 0]])
 
-    os.makedirs('../experiments/{}'.format(exp_name), exist_ok=True)
-    with open('../experiments/data_path.txt', "w") as data_path:
-        data_path.write(ds_param_files_path)
-
+            os.makedirs('../experiments/{}'.format(exp_name), exist_ok=True)
+            with open('../experiments/{}/data_path.txt'.format(exp_name), "w") as data_path:
+                data_path.write(ds_param_files_path)
+            model_name = '_'.join(['_'.join([k, str(v)]) for k, v in model_param.items()])
+            if model_param['model'] == 'autoxgb':
+                run_autoxgb(exp_name, model_param, ds_param_files_path, col_names)
+            elif model_param['model'] == 'h2o':
+                run_h2o(exp_name, model_param, ds_param_files_path, col_names)
 
 
 def main():
