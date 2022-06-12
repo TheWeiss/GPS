d3092dd1c1c4720b7ad0e80e392be9ad209bc239
The Weiss ;) <amitdweiss@gmail.com>
2022-06-12 13:26:32 +0300

    added empty range file option

--- gps_lib/experiment.py
+++ gps_lib/experiment.py
@@ -150,17 +150,12 @@ def run_exp(dataset: ds.MICDataSet, model_param, ds_param=None, antibiotic=None,
             for species_j in species:
                 run_exp(dataset, model_param, ds_param, antibiotic, species_j)
         else:
-            # try:
-            train, test, range_X, range_y, col_names, ds_param_files_path, antibiotic_name, species_name, cv = dataset.generate_dataset(
+            try:
+                train, test, range_X, range_y, col_names, ds_param_files_path, antibiotic_name, species_name, cv = dataset.generate_dataset(
                     ds_param, antibiotic, species)
-            # except SpecAntiNotExistError as e:
-            #     print(type(e))
-            #     print(e)
-            #     return -1
-            # except Exception as e:
-            #     print(type(e))
-            #     print("Unexpected error in :", species, antibiotic, e)
-            #     return -1
+            except Exception as e:
+                print(e)
+                return -1
             exp_name = '_'.join([ds_param_files_path.split('/')[-3::][i] for i in [1, 2, 0]])
 
             os.makedirs('../experiments/{}'.format(exp_name), exist_ok=True)
