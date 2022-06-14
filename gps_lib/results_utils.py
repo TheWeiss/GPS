7ef4d28da3af9fb776b7e444d5b0d085ba7c05b2
The Weiss ;) <amitdweiss@gmail.com>
2022-06-15 01:46:32 +0300

    added tot_size and more to results parser

--- gps_lib/results_utils.py
+++ gps_lib/results_utils.py
@@ -100,6 +100,9 @@ def read_exp_dirs(exp_dir_path):
     results = results.apply(fill_data_param, axis=1)
     results = pd.concat(results.apply(fill_model_param, axis=1).values).reset_index(drop=True)
     results = add_exact_param_metrices(results, equal_meaning=True)
+    results['exact_size'] = results['exact_size_train'] + results['exact_size_test']
+    results['range_size'] = results['range_size_train'] + results['range_size_test']
+    results['size'] = results['exact_size'] + results['range_size']
     return results
 
 
@@ -248,138 +251,138 @@ def add_exact_metrices(results, equal_meaning=True):
 def add_exact_param_metrices(res, equal_meaning=True):
     results = res.copy()
     for i in np.arange(len(results)):
-        # try:
-        exp_name = results['exp_path'].iloc[i]
-        model_name = results['model_path'].iloc[i]
-        data_path = results['data_path'].iloc[i]
-        if results['error'].iloc[i]:
-            continue
-        with open(data_path + '/col_names.json') as json_file:
-            col_names = json.load(json_file)
-        range_y = pd.read_csv('{}/range_y.csv'.format(data_path)).set_index(col_names['id'])
-        range_y.columns = ['y_true', 'sign']
-        split_res = {}
-        for split in ['train', 'test']:
-            split_y = pd.read_csv('{}/{}.csv'.format(data_path, split)).rename(columns={"Unnamed: 0": col_names['id']})[
-                [
-                    col_names['id'],
-                    col_names['label'],
-                ]].set_index(col_names['id'])
-            split_y.columns = ['y_true']
+        try:
+            exp_name = results['exp_path'].iloc[i]
+            model_name = results['model_path'].iloc[i]
+            data_path = results['data_path'].iloc[i]
+            if results['error'].iloc[i]:
+                continue
+            with open(data_path + '/col_names.json') as json_file:
+                col_names = json.load(json_file)
+            range_y = pd.read_csv('{}/range_y.csv'.format(data_path)).set_index(col_names['id'])
+            range_y.columns = ['y_true', 'sign']
+            split_res = {}
+            for split in ['train', 'test']:
+                split_y = pd.read_csv('{}/{}.csv'.format(data_path, split)).rename(columns={"Unnamed: 0": col_names['id']})[
+                    [
+                        col_names['id'],
+                        col_names['label'],
+                    ]].set_index(col_names['id'])
+                split_y.columns = ['y_true']
+                if results['model'].iloc[i] == 'autoxgb':
+                    split_preds = pd.read_csv(
+                        '../experiments/{}/{}/{}_preds.csv'.format(exp_name, model_name, split)).set_index(col_names['id'])
+                    split_preds.columns = ['y_pred']
+                    split_res_i = split_preds.merge(split_y, left_index=True, right_index=True, how='inner')
+                elif results['model'].iloc[i] == 'h2o':
+                    print('h2o not implemented yet')
+
+                split_res_i.loc[set(split_res_i.index) - set(range_y.index)]
+
+                split_res_i['y_true'] = np.round(split_res_i['y_true'])
+                min_true = split_res_i['y_true'].min()
+                max_true = split_res_i['y_true'].max(axis=0)
+                split_res_i['y_pred'] = split_res_i['y_pred'].clip(lower=min_true, upper=max_true)
+                split_res_i['residual'] = split_res_i['y_true'] - split_res_i['y_pred']
+                split_res_i['y_pred'] = np.round(split_res_i['y_pred'])
+                split_res_i['round_residual'] = split_res_i['y_true'] - split_res_i['y_pred']
+                split_res_i['error'] = split_res_i['round_residual'].abs() < 1
+                split_res_i['error2'] = split_res_i['round_residual'].abs() < 2
+                split_res[split] = split_res_i
+
+            regression_res = pd.DataFrame({
+                'exact RMSE': [np.sqrt(split_data['residual'].pow(2).mean()) for split_data in split_res.values()],
+                'exact_rounded RMSE': [np.sqrt(split_data['round_residual'].pow(2).mean()) for split_data in
+                                       split_res.values()],
+                'exact_accuracy': [split_data['error'].mean() for split_data in split_res.values()],
+                'exact_accuracy2': [split_data['error2'].mean() for split_data in split_res.values()],
+            }, index=['train', 'test'])
+
             if results['model'].iloc[i] == 'autoxgb':
-                split_preds = pd.read_csv(
-                    '../experiments/{}/{}/{}_preds.csv'.format(exp_name, model_name, split)).set_index(col_names['id'])
-                split_preds.columns = ['y_pred']
-                split_res_i = split_preds.merge(split_y, left_index=True, right_index=True, how='inner')
+                range_preds = pd.read_csv('../experiments/{}/{}/range_preds.csv'.format(exp_name, model_name))
+                if len(range_preds) == 0:
+                    range_preds = pd.DataFrame({col_names['id']: [], 'measurment': []}, index=[])
+                range_preds = range_preds.set_index(col_names['id'])
+                range_preds.columns = ['y_pred']
+                range_res = range_preds.merge(range_y, left_index=True, right_index=True, how='inner')
             elif results['model'].iloc[i] == 'h2o':
                 print('h2o not implemented yet')
 
-            split_res_i.loc[set(split_res_i.index) - set(range_y.index)]
+            range_res['y_true'] = np.round(range_res['y_true'])
+            range_res['updated_y_true'] = np.nan
+            range_res['updated_sign'] = np.nan
+            if not equal_meaning:
+                range_res.loc[range_res['sign'] == '>=', 'updated_y_true'] = range_res['y_true'] - 1
+                range_res.loc[range_res['sign'] == '<=', 'updated_y_true'] = range_res['y_true'] + 1
+            range_res.loc[range_res['sign'] == '>=', 'updated_sign'] = '>'
+            range_res.loc[range_res['sign'] == '<=', 'updated_sign'] = '<'
+            range_res.loc[:, 'updated_y_true'].fillna(range_res['y_true'], inplace=True)
+            range_res.loc[:, 'updated_sign'].fillna(range_res['sign'], inplace=True)
 
-            split_res_i['y_true'] = np.round(split_res_i['y_true'])
-            min_true = split_res_i['y_true'].min()
-            max_true = split_res_i['y_true'].max(axis=0)
-            split_res_i['y_pred'] = split_res_i['y_pred'].clip(lower=min_true, upper=max_true)
-            split_res_i['residual'] = split_res_i['y_true'] - split_res_i['y_pred']
-            split_res_i['y_pred'] = np.round(split_res_i['y_pred'])
-            split_res_i['round_residual'] = split_res_i['y_true'] - split_res_i['y_pred']
-            split_res_i['error'] = split_res_i['round_residual'].abs() < 1
-            split_res_i['error2'] = split_res_i['round_residual'].abs() < 2
-            split_res[split] = split_res_i
+            range_res.loc[range_res['updated_sign'] == '>', 'error'] = (
+                    range_res['y_pred'] > range_res['updated_y_true'])
+            range_res.loc[range_res['updated_sign'] == '<', 'error'] = (
+                    range_res['y_pred'] < range_res['updated_y_true'])
+            range_res.loc[range_res['updated_sign'] == '>', 'error2'] = (
+                    range_res['y_pred'] > range_res['updated_y_true'] - 1)
+            range_res.loc[range_res['updated_sign'] == '<', 'error2'] = (
+                    range_res['y_pred'] < range_res['updated_y_true'] + 1)
 
-        regression_res = pd.DataFrame({
-            'exact RMSE': [np.sqrt(split_data['residual'].pow(2).mean()) for split_data in split_res.values()],
-            'exact_rounded RMSE': [np.sqrt(split_data['round_residual'].pow(2).mean()) for split_data in
-                                   split_res.values()],
-            'exact_accuracy': [split_data['error'].mean() for split_data in split_res.values()],
-            'exact_accuracy2': [split_data['error2'].mean() for split_data in split_res.values()],
-        }, index=['train', 'test'])
+            train_range_res = range_res.loc[set(range_res.index).intersection(set(split_res['train'].index))]
+            test_range_res = range_res.loc[set(range_res.index) - set(split_res['train'].index)]
 
-        if results['model'].iloc[i] == 'autoxgb':
-            range_preds = pd.read_csv('../experiments/{}/{}/range_preds.csv'.format(exp_name, model_name))
-            if len(range_preds) == 0:
-                range_preds = pd.DataFrame({col_names['id']: [], 'measurment': []}, index=[])
-            range_preds = range_preds.set_index(col_names['id'])
-            range_preds.columns = ['y_pred']
-            range_res = range_preds.merge(range_y, left_index=True, right_index=True, how='inner')
-        elif results['model'].iloc[i] == 'h2o':
-            print('h2o not implemented yet')
+            for key, res in {'train': train_range_res, 'test': test_range_res}.items():
+                range_confusion = res.groupby(by=['sign', 'y_true'])['error'].agg(['count', 'sum']).replace(True, 1)
+                range_confusion['perc'] = range_confusion['sum'] / range_confusion['count']
+                range_confusion.columns = ['range_total', 'range_true', 'range_accuracy']
+                range_confusion = pd.DataFrame(range_confusion.stack()).T.swaplevel(i=2, j=0, axis=1)
+                range_confusion.index = [key]
+                regression_res = pd.concat([regression_res, range_confusion], axis=1)
 
-        range_res['y_true'] = np.round(range_res['y_true'])
-        range_res['updated_y_true'] = np.nan
-        range_res['updated_sign'] = np.nan
-        if not equal_meaning:
-            range_res.loc[range_res['sign'] == '>=', 'updated_y_true'] = range_res['y_true'] - 1
-            range_res.loc[range_res['sign'] == '<=', 'updated_y_true'] = range_res['y_true'] + 1
-        range_res.loc[range_res['sign'] == '>=', 'updated_sign'] = '>'
-        range_res.loc[range_res['sign'] == '<=', 'updated_sign'] = '<'
-        range_res.loc[:, 'updated_y_true'].fillna(range_res['y_true'], inplace=True)
-        range_res.loc[:, 'updated_sign'].fillna(range_res['sign'], inplace=True)
+            regression_res_cleaned = pd.DataFrame({})
+            for col in regression_res.columns:
+                if len(regression_res[[col]].columns) > 1:
+                    regression_res_cleaned[col] = regression_res[[col]].iloc[:, 0].fillna(
+                        regression_res[[col]].iloc[:, 1])
+                else:
+                    regression_res_cleaned[col] = regression_res[[col]]
+            regression_res = regression_res_cleaned
+            regression_res['range_accuracy'] = [
+                train_range_res['error'].mean(),
+                test_range_res['error'].mean(),
+            ]
+            regression_res['range_accuracy'].fillna(0, inplace=True)
+            regression_res['range_accuracy2'] = [
+                train_range_res['error2'].mean(),
+                test_range_res['error2'].mean(),
+            ]
+            regression_res['range_accuracy2'].fillna(0, inplace=True)
+            regression_res['range_size'] = [
+                len(train_range_res),
+                len(test_range_res),
+            ]
+            regression_res['range_size'].fillna(0, inplace=True)
+            regression_res['exact_size'] = [len(split_data) for split_data in split_res.values()]
+            regression_res['exact_size'].fillna(0, inplace=True)
+            regression_res['accuracy'] = (regression_res['exact_accuracy'].fillna(0) * regression_res[
+                'exact_size'].fillna(0) \
+                                          + regression_res['range_accuracy'] * regression_res['range_size']) \
+                                         / (regression_res['range_size'] + regression_res['exact_size'].fillna(0))
+            regression_res['essential_agreement'] = (regression_res['exact_accuracy2'].fillna(0) * regression_res[
+                'exact_size'].fillna(0) \
+                                                     + regression_res['range_accuracy2'] * regression_res['range_size']) \
+                                                    / (regression_res['range_size'] + regression_res[
+                'exact_size'].fillna(0))
 
-        range_res.loc[range_res['updated_sign'] == '>', 'error'] = (
-                range_res['y_pred'] > range_res['updated_y_true'])
-        range_res.loc[range_res['updated_sign'] == '<', 'error'] = (
-                range_res['y_pred'] < range_res['updated_y_true'])
-        range_res.loc[range_res['updated_sign'] == '>', 'error2'] = (
-                range_res['y_pred'] > range_res['updated_y_true'] - 1)
-        range_res.loc[range_res['updated_sign'] == '<', 'error2'] = (
-                range_res['y_pred'] < range_res['updated_y_true'] + 1)
+            regression_res = pd.DataFrame(regression_res.unstack()).T
 
-        train_range_res = range_res.loc[set(range_res.index).intersection(set(split_res['train'].index))]
-        test_range_res = range_res.loc[set(range_res.index) - set(split_res['train'].index)]
-
-        for key, res in {'train': train_range_res, 'test': test_range_res}.items():
-            range_confusion = res.groupby(by=['sign', 'y_true'])['error'].agg(['count', 'sum']).replace(True, 1)
-            range_confusion['perc'] = range_confusion['sum'] / range_confusion['count']
-            range_confusion.columns = ['range_total', 'range_true', 'range_accuracy']
-            range_confusion = pd.DataFrame(range_confusion.stack()).T.swaplevel(i=2, j=0, axis=1)
-            range_confusion.index = [key]
-            regression_res = pd.concat([regression_res, range_confusion], axis=1)
-
-        regression_res_cleaned = pd.DataFrame({})
-        for col in regression_res.columns:
-            if len(regression_res[[col]].columns) > 1:
-                regression_res_cleaned[col] = regression_res[[col]].iloc[:, 0].fillna(
-                    regression_res[[col]].iloc[:, 1])
-            else:
-                regression_res_cleaned[col] = regression_res[[col]]
-        regression_res = regression_res_cleaned
-        regression_res['range_accuracy'] = [
-            train_range_res['error'].mean(),
-            test_range_res['error'].mean(),
-        ]
-        regression_res['range_accuracy'].fillna(0, inplace=True)
-        regression_res['range_accuracy2'] = [
-            train_range_res['error2'].mean(),
-            test_range_res['error2'].mean(),
-        ]
-        regression_res['range_accuracy2'].fillna(0, inplace=True)
-        regression_res['range_size'] = [
-            len(train_range_res),
-            len(test_range_res),
-        ]
-        regression_res['range_size'].fillna(0, inplace=True)
-        regression_res['exact_size'] = [len(split_data) for split_data in split_res.values()]
-        regression_res['exact_size'].fillna(0, inplace=True)
-        regression_res['accuracy'] = (regression_res['exact_accuracy'].fillna(0) * regression_res[
-            'exact_size'].fillna(0) \
-                                      + regression_res['range_accuracy'] * regression_res['range_size']) \
-                                     / (regression_res['range_size'] + regression_res['exact_size'].fillna(0))
-        regression_res['essential_agreement'] = (regression_res['exact_accuracy2'].fillna(0) * regression_res[
-            'exact_size'].fillna(0) \
-                                                 + regression_res['range_accuracy2'] * regression_res['range_size']) \
-                                                / (regression_res['range_size'] + regression_res[
-            'exact_size'].fillna(0))
-
-        regression_res = pd.DataFrame(regression_res.unstack()).T
-
-        regression_res.columns = ['{}_{}'.format(col[0], col[1])
-                                  for col in regression_res.columns]
-        regression_res.index = [i]
-        regression_res['all_results_generated'] = True
-        # except:
-        #     regression_res = pd.DataFrame({}, index=[0])
-        #     regression_res['exp_done'] = False
+            regression_res.columns = ['{}_{}'.format(col[0], col[1])
+                                      for col in regression_res.columns]
+            regression_res.index = [i]
+            regression_res['exp_done'] = True
+        except (FileNotFoundError, Exception):
+            regression_res = pd.DataFrame({}, index=[0])
+            regression_res['exp_done'] = False
         regression_res.index = [i]
         results = pd.concat([results, pd.DataFrame(columns=regression_res.columns)])
         results.update(regression_res)
