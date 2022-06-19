668f358ebb04300210c9f9992be5e5307e4940a8
The Weiss ;) <amitdweiss@gmail.com>
2022-06-19 23:58:15 +0300

    added OSError for h2o error to be catched

--- gps_lib/results_utils.py
+++ gps_lib/results_utils.py
@@ -442,7 +442,7 @@ def add_metrices(res, equal_meaning=True, range_conf=False):
                                       for col in regression_res.columns]
             regression_res.index = [i]
             regression_res['exp_done'] = True
-        except FileNotFoundError:
+        except (FileNotFoundError, OSError):
             regression_res = pd.DataFrame({}, index=[0])
             regression_res['exp_done'] = False
         regression_res.index = [i]
