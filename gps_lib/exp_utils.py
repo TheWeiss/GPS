0dba2bb2d3438b9cedbf27f8148d9d73a4456f01
The Weiss ;) <amitdweiss@gmail.com>
2022-10-25 22:34:59 +0300

    debugging print of breakpoints

--- gps_lib/exp_utils.py
+++ gps_lib/exp_utils.py
@@ -107,7 +107,7 @@ def print_anti_measure(all_ASR, species, anti_index, need_log=False, path=None):
         print(s, r)
     if not np.isnan(s):
         print(hist_range[:-1]+0.5)
-        print(np.where(hist_range[:-1]+0.5 == s)
+        print(np.where(hist_range[:-1]+0.5 == s))
         plt.axvline(x=np.where(hist_range[:-1]+0.5 == s), color='g', ls=':', label='S breakpoint')
     if not np.isnan(r):
         plt.axvline(x=np.where(hist_range == r), color='r', ls=':', label='R breakpoint')
